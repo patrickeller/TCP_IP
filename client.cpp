242bbd1d5bbf42c81caff320b37767e759445aa8
Patrick Eller <patrick.eller@werbewind.com>
2022-10-25 17:30:43 +0200

    init

---
+++ client.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#include <winsock2.h>
+
+int main()
+{
+    std::string ipAddress = "192.168.123.77"; // IP Address of the server
+    int port = 54000;                         // Listening port # on the server
+
+    // Initialize WinSock
+    WSAData data;
+    WORD ver = MAKEWORD(2, 2);
+    int res = WSAStartup(ver, &data);
+    if (res == 0)
+    {
+        std::cout << "WSA done..." << std::endl;
+    }
+    else
+    {
+        std::cerr << "WSA failed!" << std::endl;
+        return -1;
+    }
+
+    // Create socket
+    SOCKET clientConnect = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientConnect != INVALID_SOCKET)
+    {
+        std::cout << "Socket done..." << std::endl;
+    }
+    else
+    {
+        std::cerr << "Socket failed!" << std::endl;
+        WSACleanup();
+        return -1;
+    }
+
+    // Fill in a hint structure
+    sockaddr_in info;
+    info.sin_family = AF_INET;
+    info.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress.c_str(), &info.sin_addr);
+
+    // Connect to server
+    int server = connect(clientConnect, (sockaddr *)&info, sizeof(info));
+
+    if (server != SOCKET_ERROR)
+    {
+        std::cout << "Connect done..." << std::endl;
+    }
+    else
+    {
+        std::cerr << "Connect failed!" << std::endl;
+        closesocket(clientConnect);
+        WSACleanup();
+        return -1;
+    }
+
+    // Do-while loop to send and receive data
+    char buf[4096];
+    std::string userInput;
+
+    do
+    {
+        // Prompt the user for some text
+        std::cout << "> ";
+        std::getline(std::cin, userInput);
+
+        if (userInput.size() > 0) // Make sure the user has typed in something
+        {
+            // Send the text
+            int sendResult = send(clientConnect, userInput.c_str(), userInput.size() + 1, 0);
+            if (sendResult != SOCKET_ERROR)
+            {
+                // Wait for response
+                ZeroMemory(buf, 4096);
+                int bytesReceived = recv(clientConnect, buf, 4096, 0);
+                if (bytesReceived > 0)
+                {
+                    // Echo response to console
+                    std::cout << "SERVER> " << std::string(buf, 0, bytesReceived) << std::endl;
+                }
+            }
+        }
+
+    } while (userInput.size() > 0);
+
+    // Gracefully close down everything
+    closesocket(clientConnect);
+    WSACleanup();
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,68 @@
+#pragma comment(lib, "ws2_32.lib")
+
+#include <cstdio>
+#include <iostream>
+#include <winsock2.h>
+
+
+int main()
+{
+
+    long res;
+
+    WSADATA wsaData;
+
+    res = WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+    if (res == 0)
+        std::cout << "WSA done..." << std::endl;
+    else
+        std::cout << "WSAStartup failed!" << std::endl;
+
+    SOCKET slisten, client;
+
+    slisten = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (slisten != INVALID_SOCKET)
+        std::cout << "Socket done..." << std::endl;
+    else
+        std::cout << "Socket failed!" << std::endl;
+
+    sockaddr_in info;
+    info.sin_addr.s_addr = inet_addr("192.168.123.77");
+    info.sin_family = AF_INET;
+    info.sin_port = htons(54000);
+    int infolen = sizeof(info);
+
+    res = bind(slisten, (struct sockaddr *)&info, infolen);
+
+    if (slisten != SOCKET_ERROR)
+        std::cout << "Bind done..." << std::endl;
+    else
+        std::cout << "Bind failed!" << std::endl;
+
+    res = listen(slisten, SOMAXCONN);
+
+    if (res != SOCKET_ERROR)
+        std::cout << "Listen done..." << std::endl;
+    else
+        std::cout << "Listen failed!" << std::endl;
+
+    sockaddr_in clientinfo;
+    int clientinfolen = sizeof(clientinfo);
+
+    while (1)
+    {
+        client = accept(slisten, (struct sockaddr *)&clientinfo, &clientinfolen);
+        if (client != SOCKET_ERROR)
+            std::cout << "Client accepted: " << inet_ntoa(clientinfo.sin_addr) << " on Port: " << ntohs(clientinfo.sin_port) << "..." << std::endl;
+        else
+            std::cout << "Listen failed!" << std::endl;
+    }
+
+    closesocket(client);
+    closesocket(slisten);
+    WSACleanup();
+
+    return 0;
+}
